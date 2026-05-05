efb0a50a075749512180d8c59bb9ce2bdaa3098b
Rishitha Goud <rishithagoud1706@gmail.com>
2026-05-05 11:17:05 +0530

    completed header and navbar

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector(".menu");
+const nav = document.querySelector(".nav");
+
+menu.addEventListener("click", () => {
+    if (nav.style.display === "flex") {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "flex";
+        nav.style.flexDirection = "column";
+    }
+});
