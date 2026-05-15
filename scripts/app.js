104c3f858945387bf1658587428b945ad806fa51
Rishitha Goud <rishithagoud1706@gmail.com>
2026-05-15 11:01:40 +0530

    initial project setup

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,1 @@
-const menu = document.querySelector(".menu");
-const nav = document.querySelector(".nav");
-
-menu.addEventListener("click", () => {
-    if (nav.style.display === "flex") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "flex";
-        nav.style.flexDirection = "column";
-    }
-});
+console.log("E-Commerce Website Loaded");
